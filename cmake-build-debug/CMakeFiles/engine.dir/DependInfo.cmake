ecbed38cfd3de8fd1dcbcb1c081393101bf95618
Arne De Peuter <arne@depeuter.org>
2023-03-10 15:55:53 +0100

    readme

---
+++ cmake-build-debug/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Include/easy_image.cc" "CMakeFiles/engine.dir/Include/easy_image.cc.obj" "gcc" "CMakeFiles/engine.dir/Include/easy_image.cc.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Include/ini_configuration.cc" "CMakeFiles/engine.dir/Include/ini_configuration.cc.obj" "gcc" "CMakeFiles/engine.dir/Include/ini_configuration.cc.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Include/l_parser.cc" "CMakeFiles/engine.dir/Include/l_parser.cc.obj" "gcc" "CMakeFiles/engine.dir/Include/l_parser.cc.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Include/vector3d.cc" "CMakeFiles/engine.dir/Include/vector3d.cc.obj" "gcc" "CMakeFiles/engine.dir/Include/vector3d.cc.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Objects/Calculator.cpp" "CMakeFiles/engine.dir/Objects/Calculator.cpp.obj" "gcc" "CMakeFiles/engine.dir/Objects/Calculator.cpp.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Objects/LSystem2D.cpp" "CMakeFiles/engine.dir/Objects/LSystem2D.cpp.obj" "gcc" "CMakeFiles/engine.dir/Objects/LSystem2D.cpp.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Objects/Object3D.cpp" "CMakeFiles/engine.dir/Objects/Object3D.cpp.obj" "gcc" "CMakeFiles/engine.dir/Objects/Object3D.cpp.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/Objects/Wireframe.cpp" "CMakeFiles/engine.dir/Objects/Wireframe.cpp.obj" "gcc" "CMakeFiles/engine.dir/Objects/Wireframe.cpp.obj.d"
+  "C:/Users/arne/Documents/2022-2023/s2/ComputerGraphics/myEngine/engine.cc" "CMakeFiles/engine.dir/engine.cc.obj" "gcc" "CMakeFiles/engine.dir/engine.cc.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
